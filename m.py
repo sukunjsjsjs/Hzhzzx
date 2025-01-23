138176c806831578ddc303f37fe9f10a45a26fea
sukunjsjsjs <dddsd452256@gmail.com>
2025-01-23 21:27:23 +0530

    Update m.py

--- m.py
+++ m.py
@@ -8,7 +8,7 @@ import datetime
 import os
 
 # insert your Telegram bot token here
-bot = telebot.TeleBot('7256838089:AAHWbH-d7klrvMqgV1NFqYBD9uL_ThcDXTc')
+bot = telebot.TeleBot('7313188191:AAHy9sQnzYMxzv1dMfGyQ9zOH6oTCfLUhcQ')
 
 # Admin user IDs
 admin_id = ["5993665056"]
